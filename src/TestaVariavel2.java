8f147f3f704287087561384bde3a40c12358c1b5
m240840 <bruno.noberto@sempreit.com.br>
2021-01-26 16:15:24 -0300

    adicionado char e String

---
+++ src/TestaVariavel2.java
@@ -0,0 +1,19 @@
+
+public class TestaVariavel2 {
+
+	public static void main(String[] args) {
+		
+		// Caracteres e String
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 65; // valor referente a tabela unicode
+		System.out.println(valor);
+		
+		String palavra = "alura cursos online de tecnologia";
+		System.out.println(palavra);
+		
+		palavra += 2020;
+		System.out.println(palavra);
+	}
+}
