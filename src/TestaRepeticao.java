2c7a503bc10e5127cb7d1c3c3828085551034891
m240840 <bruno.noberto@sempreit.com.br>
2021-01-27 08:52:34 -0300

    adicionado laço de repeticao

---
+++ src/TestaRepeticao.java
@@ -0,0 +1,20 @@
+
+public class TestaRepeticao {
+
+	public static void main(String[] args) {
+		
+		// While
+		int contador = 0;
+		
+		while(contador <= 10) {
+			System.out.println("Valor do contador no while: " + contador);
+			contador++;
+		}
+		
+		// For
+		for(int conttador = 0; contador <= 10; contador++) {
+			System.out.println(contador);
+		}
+	}
+
+}
